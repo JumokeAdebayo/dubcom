97451d3707934c7f0f93f7f9ca21267d13af138f
JumokeAdebayo <olajumoke97@yahoo.com>
2020-04-15 16:22:32 +0100

    Updated Settings

--- spwproject/settings.py
+++ spwproject/settings.py
@@ -199,7 +199,7 @@ AWS_S3_REGION_NAME ='eu-west-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
